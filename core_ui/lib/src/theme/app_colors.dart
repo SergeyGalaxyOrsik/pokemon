a3689a7ef006bdc4e65deb762f515d90cd0e0083
Sergey Orsik <93079311+SergeyGalaxyOrsik@users.noreply.github.com>
2023-06-24 19:07:37 +0300

    theme commit

--- core_ui/lib/src/theme/app_colors.dart
+++ core_ui/lib/src/theme/app_colors.dart
@@ -1,29 +1,37 @@
 part of core_ui;
 
-abstract class AppColors {
-  factory AppColors.of(BuildContext context) {
-    final Brightness brightness = Theme.of(context).brightness;
-    return brightness == Brightness.light
-        ? const LightColors()
-        : const DarkColors();
-  }
+class AppColors {
+  
 
-  Color get primaryBg;
+  // Primary color
+  static const Color primaryPok = Color(0xFFdc0a2d);
 
-  Color get white;
+  // Pokemon Type colors
+  static const Color bug = Color(0xFFa7b723);
+  static const Color dark = Color(0xFF75574c);
+  static const Color dragon = Color(0xFF7037ff);
+  static const Color electric = Color(0xFFf9cf30);
+  static const Color fairy = Color(0xFFe69eac);
+  static const Color fighting = Color(0xFFc12239);
+  static const Color fire = Color(0xFFf57d31);
+  static const Color flying = Color(0xFFa891ec);
+  static const Color ghost = Color(0xFF70559b);
+  static const Color normal = Color(0xFFaaa67f);
+  static const Color grass = Color(0xFF74cb48);
+  static const Color ground = Color(0xFFdec16b);
+  static const Color ice = Color(0xFF9ad6df);
+  static const Color poison = Color(0xFFa43e93);
+  static const Color psychic = Color(0xFFfb5584);
+  static const Color rock = Color(0xFFb69e31);
+  static const Color steel = Color(0xFFb7b9d0);
+  static const Color water = Color(0xFF6493eb);
+
+  // Grayscale
+  static const Color greyDark = Color(0xFF212121);
+  static const Color greyMedium = Color(0xFF666666);
+  static const Color greyLight = Color(0xFFe0e0e0);
+  static const Color greyBackground = Color(0xFFefefef);
+  static const Color greyWhite = Color(0xFFffffff);
+
 }
 
-class DarkColors extends LightColors {
-  const DarkColors();
-}
-
-class LightColors implements AppColors {
-  const LightColors();
-
-  @override
-  // RGBO(236, 239, 241, 1)
-  Color get primaryBg => const Color(0xFFeceff1);
-
-  @override
-  Color get white => const Color.fromRGBO(255, 255, 255, 1);
-}
--- core_ui/lib/src/theme/app_fonts.dart
+++ core_ui/lib/src/theme/app_fonts.dart
@@ -2,7 +2,58 @@ part of core_ui;
 
 class AppFonts {
   static TextStyle normal13 = const TextStyle(
+    fontFamily: 'Poppins',
     fontWeight: FontWeight.w400,
     fontSize: 13,
   );
+
+  static TextStyle body14 = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+  
+  static TextStyle body10 = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    fontSize: 10,
+  );
+  
+  static TextStyle body12 = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+  );
+  
+  static TextStyle caption = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+    fontSize: 8,
+  );
+  
+  static TextStyle headlineBold = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+    fontSize: 24,
+  );
+  
+  static TextStyle subtitleBold10 = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+    fontSize: 10,
+    color: AppColors.greyMedium
+  );
+  
+  static TextStyle subtitleBold14 = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+    fontSize: 14,
+  );
+  
+  static TextStyle subtitleBold12 = const TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700,
+    fontSize: 12,
+  );
+
 }
