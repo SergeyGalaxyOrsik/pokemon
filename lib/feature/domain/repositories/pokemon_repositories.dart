aec12a9baf6af6a2efb1ffee2524597fa6e35458
Sergey Orsik <93079311+SergeyGalaxyOrsik@users.noreply.github.com>
2023-06-28 20:07:20 +0300

    commit

--- lib/feature/domain/repositories/pokemon_repositories.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:pokemon/core/error/failure.dart';
-import 'package:pokemon/feature/domain/entities/pokemon_entity.dart';
-
-abstract class PokemonRepository {
-  Future<Either<Failure,List<PokemonEntity>>> getAllPokemons(int offset);
-}
